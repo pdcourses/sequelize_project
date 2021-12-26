d6b69228321384074a521c18ca2683a57a40796d
julietvt <freshitpt@gmail.com>
2021-12-26 13:32:49 +0200

    create env

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -1,10 +1,12 @@
+require('dotenv/config');
+
 module.exports = {
   development: {
-    username: 'vasya',
-    password: 'admin',
-    database: 'todo_test',
-    host: 'localhost',
-    port: 5432,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'postgres',
   },
   test: {},
