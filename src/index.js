bc65f5845e56557dce64fad511834d1c68e14dad
julietvt <freshitpt@gmail.com>
2022-01-08 13:31:34 +0200

    express hello world

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import express from 'express';
+const app = express();
+const port = process.env.PORT || 5000;
 
+// req -обьект запроса , res - объект ответа
+app.get('/', (req, res) => res.send('Hello!!!'));
+
+app.listen(port, () => console.log(`App listening on port ${port}`));
